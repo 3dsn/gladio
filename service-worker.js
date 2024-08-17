778053444510e1fe34558e13dbd62bf418fb417d
Edson Barbosa <3dsn@users.noreply.github.com>
2024-08-17 01:14:20 -0300

    Add files via upload
    Gladio eh 15 first deploy

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-cache')
+            .then(cache => {
+                return cache.addAll([
+                    '/livreto15.svg',
+                    // Adicione outros arquivos que você deseja armazenar em cache
+                ]);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
